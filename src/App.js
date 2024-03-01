6d44cd016ac2613ec4a6e356ba91b1559d9ff7b5
Manoj-kumar-T <manojk.t.it.2020@snsct.org>
2024-03-01 12:51:19 +0530

    timer-app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import CountDown from "./CoutDown";
 
 function App() {
+  let onTimesup = () => {
+    alert(`time's up!`);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>--- A COUNTDOWN TIMER WITH REACT---</p>
+        <div>
+          <CountDown onTimesup={onTimesup} duration={6} />
+        </div>
       </header>
     </div>
   );
